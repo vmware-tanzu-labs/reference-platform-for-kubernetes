2d0c707e0ca2ab41305cbd37ef0d18bd2799d476
Dustin Scott <sdustin@vmware.com>
2021-05-21 12:41:20 -0500

    make ansible-lint strict

--- ci/scripts/lint-ansible.sh
+++ ci/scripts/lint-ansible.sh
@@ -2,12 +2,23 @@
 # Copyright 2006-2021 VMware, Inc.
 # SPDX-License-Identifier: MIT
 
+# set the exit code and adjust if there are failures
+EXIT_CODE=0
+
 echo 'linting ansible using ansible-lint rules at .ansible-lint'
 
 # lint each role independently
 for ROLE in `find roles/ -mindepth 1 -maxdepth 2 -type d`; do
   ansible-lint "${ROLE}/" -vvvvvvvvvvvvvv -R -r ./.ansible-lint-rules
+  if [[ $? != 0 ]]; then
+    EXIT_CODE=$?
+  fi
 done
 
 # lint the top-level playbook
 ansible-lint site.yaml -vvvvvvvvvvvvvv -R -r ./.ansible-lint-rules
+if [[ $? != 0 ]]; then
+  EXIT_CODE=$?
+fi
+
+exit ${EXIT_CODE}
